19736216c9a9efff9c22eb9bdac45707b4e52522
“hillmclean” <hilldmc@gmail.com>
2020-11-15 22:09:47 -0800

    add grid to front page

--- themes/cjmedia/front-page.php
+++ themes/cjmedia/front-page.php
@@ -9,18 +9,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+	
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<div class="front-logo">
-					<p>hello</p>
+				<div class="front-grid">
+					<div class="front-logo"></div>
 				</div>
-
 			
 
-			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
