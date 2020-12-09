a7e3dcff69b00ce20d408759ea7aa2b2c9cbc15f
“hillmclean” <hilldmc@gmail.com>
2020-12-08 23:24:57 -0800

    changes template header for front page to exclude menu

--- themes/cjmedia/front-page.php
+++ themes/cjmedia/front-page.php
@@ -5,7 +5,7 @@
  * @package Chelsea_Jade_Media_Theme
  */
 
-get_header(); ?>
+get_template_part( 'template-parts/header', 'page-front' ); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
