fb207303a954cc28cbdb8bc6a2eb61d19c062c8f
“hillmclean” <hilldmc@gmail.com>
2021-02-21 21:05:12 -0800

    adjusts font on box images on front page. adds hummingbird logo to header

--- themes/cjmedia/header.php
+++ themes/cjmedia/header.php
@@ -24,7 +24,7 @@
 			<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
 				<a href="<?php echo get_home_url() ?>">
-				    <img src="<?php echo get_template_directory_uri() . "/build/assets/logos/ChelseaJadeMedia-logo.svg"?>"></img>
+				    <img src="<?php echo get_template_directory_uri() . "/build/assets/logos/hummingbird.png"?>"></img>
 				</a>
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					
