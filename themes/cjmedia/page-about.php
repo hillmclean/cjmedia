55b0cf6779e7b18e4f95eccfb91a1a6c29c3a71e
“hillmclean” <hilldmc@gmail.com>
2021-02-21 22:56:34 -0800

    adds 2/3 colour boxes on about page

--- themes/cjmedia/page-about.php
+++ themes/cjmedia/page-about.php
@@ -16,7 +16,10 @@ get_header(); ?>
 		</div>
 
 		<div class="about-grid">
+			
 			<div class="image-1" style="background: url(<?php  echo the_field('image_1'); ?>); background-size: cover; background-position: center;"></div>
+
+			<div class="image-1-blue-box"></div>
 
 			<div class="content">
 				<?php while ( have_posts() ) : the_post(); ?>
@@ -30,6 +33,8 @@ get_header(); ?>
 			<div class="image-2" style="background: url(<?php  echo the_field('image_2'); ?>); background-size: cover; background-position: center;"></div>
 
 			<div class="image-3" style="background: url(<?php  echo the_field('image_3'); ?>); background-size: cover; background-position: center;"></div>
+
+			<div class="image-1-red-box"></div>
 
 			<div class="image-4" style="background: url(<?php  echo the_field('image_4'); ?>); background-size: cover; background-position: center;"></div>
 
